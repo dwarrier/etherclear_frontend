815637042c9229bc0b6304a3b66190129e5653e3
etherclearapp <etherclearapp@protonmail.com>
2019-06-23 18:08:39 -0700

    Setup workflow for first release Current versions: 1561304579-ropsten, 1561304579-main Ropsten registry: 0xDD2aE0e664350A88919d47Ae00ae7d7e80Cda802 Mainnet registry: 0x76b832678cB887121B0eE95Cf449c7FDde5B3427 Ropsten contract: 0xFA280a863c60c260e43Ccb2D523C4Cf38DA0cB53 Mainnet contract: 0xfb252087ABD657d69e243efAba2d972432f695c8

---
+++ 1561304579/build/precache-manifest.632cafb4bca735ddd2d1e152fba62dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4491dd8bccecae23d9a3",
+    "url": "/static/css/main.60360720.chunk.css"
+  },
+  {
+    "revision": "4491dd8bccecae23d9a3",
+    "url": "/static/js/main.50482e17.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f9e97b90c6e3a09ed",
+    "url": "/static/css/2.96d3c44e.chunk.css"
+  },
+  {
+    "revision": "674f9e97b90c6e3a09ed",
+    "url": "/static/js/2.eab43096.chunk.js"
+  },
+  {
+    "revision": "e3b143a51c3b1f2e52f20a989ec7ab74",
+    "url": "/static/media/logo.e3b143a5.svg"
+  },
+  {
+    "revision": "af81063b92ae5b276d86dc7e1e4987d4",
+    "url": "/index.html"
+  }
+];
